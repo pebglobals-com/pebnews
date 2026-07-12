e2b882be8d8e1686b09ec689bec5f962f0ff8bcf
PEB News Deploy <deploy@pebnews.com>
2026-07-12 05:06:01 +0100

    Add Nigerian RSS feeds (Premium Times, Guardian, Punch, Vanguard, Daily Post, Channels TV)

---
+++ apps/api/src/db/seed_rss.sql
@@ -0,0 +1,7 @@
+INSERT INTO rss_sources (id, name, feed_url, active) VALUES
+('src_premium', 'Premium Times', 'https://www.premiumtimesng.com/feed', 1),
+('src_guardian', 'The Guardian NG', 'https://guardian.ng/feed', 1),
+('src_punch', 'Punch NG', 'https://punchng.com/feed', 1),
+('src_vanguard', 'Vanguard NG', 'https://www.vanguardngr.com/feed', 1),
+('src_dailypost', 'Daily Post NG', 'https://dailypost.ng/feed', 1),
+('src_channels', 'Channels TV', 'https://www.channelstv.com/feed', 1);
